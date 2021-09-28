9e1eaf6cfc861c92eaa16fb8379ebb8b6646af8a
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-09-28 18:20:49 +0000

    problem 2 solved

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 //Code your solutions in this file
-function fiveToOneHundred(){
+
+//problem 1
+
+/* function fiveToOneHundred(){
     for(let i = 5; i <= 100; i++) {
         console.log(i)
     }
 }
 
-fiveToOneHundred()
+*/
+//problem 2
+const multiplesOfThree = () => {
+    for(let i = 1; i < 101; i++) {
+        if(i % 3 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+multiplesOfThree()
+//fiveToOneHundred()
