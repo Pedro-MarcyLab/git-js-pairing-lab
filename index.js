5fc862b029f988a64a863f4784f8a7a4e0ec278e
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-09-28 18:26:59 +0000

    problem 3 completed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
     }
 }
 
-*/
+
 //problem 2
 const multiplesOfThree = () => {
     for(let i = 1; i < 101; i++) {
@@ -20,3 +20,15 @@ const multiplesOfThree = () => {
 
 multiplesOfThree()
 //fiveToOneHundred()
+
+
+//problem 3
+const multiplesOfThreeOrFive = () => {
+    for(let i = 1; i <= 100; i++) {
+        if((i % 3 === 0) || (i % 5 === 0)) {
+            console.log(i);
+        }
+    }
+}
+multiplesOfThreeOrFive()
+*/
